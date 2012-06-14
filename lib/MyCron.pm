c4e5fef31e6459e68086e0a423f44728016b3581
tokuhirom <tokuhirom@gmail.com>
2012-06-14 18:51:13 +0900

    mycron

--- lib/MyCron.pm
+++ lib/MyCron.pm
@@ -3,37 +3,4 @@ use strict;
 use warnings;
 use 5.008008;
 our $VERSION = '0.01';
-
-
-
 1;
-__END__
-
-=encoding utf8
-
-=head1 NAME
-
-MyCron - A module for you
-
-=head1 SYNOPSIS
-
-  use MyCron;
-
-=head1 DESCRIPTION
-
-MyCron is
-
-=head1 AUTHOR
-
-Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-Copyright (C) Tokuhiro Matsuno
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
